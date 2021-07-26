288cec2686962fe90b87c433fcd99222b53a1b4a
lhy2143 <1021703626@qq.com>
2021-07-26 16:58:31 +0800

    test

--- script/test/test.js
+++ script/test/test.js
@@ -1,7 +1,6 @@
 const bundler = require('../src')
 const fs = require('fs')
 const path = require('path')
-
 const content = bundler('./index')
 
 fs.writeFileSync(path.resolve(__dirname,'./index.bundle.js'),content,'utf-8')
